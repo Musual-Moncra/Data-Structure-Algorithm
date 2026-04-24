3634e4ec4ca7e906dea4156ed462e96c692426bc
Sỳ <148116115+Molten-2168@users.noreply.github.com>
2026-04-24 15:37:58 +0700

    baitap cua lab05 dem nop cho tuan 6

---
+++ src/SyChiKhoi/src/Project/Lab05/Tuan6/baitap02_sv.c
@@ -0,0 +1,80 @@
+/*
+Bài tập tuần 5
+Bài tập 1: Dùng Selection sort để sắp xếp danh sách sinh viên theo tăng dần của cột Name.
+Input: file students.txt
+Output: danh sách sinh viên đã được sắp xếp theo Name tăng dần
+Yêu cầu: sinh viên hoàn thành code tại vị trí có comment "YOUR CODE HERE..." trong hàm sort_by_name()
+*/
+#include <stdio.h>
+#include <string.h>
+
+// Define a Student struct
+typedef struct Student {
+    int sid;
+    char name[30];
+    float gpa;
+} Student;
+
+// print array of students
+void print_students(Student s[], int n) {
+    printf("ID\t Name\t Score\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t %s\t %.2f\n", s[i].sid, s[i].name, s[i].gpa);
+    }
+}
+
+// Dùng để đếm số lượng dòng trong file students.txt
+// tương ứng với số sinh viên
+int count_lines(char *filename) {
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) {
+        printf("Can not open file %s\n", filename);
+        return 0;
+    }
+
+    int count = 0;
+    char c;
+    while ((c = fgetc(f)) != EOF) {
+        if (c == '\n') {
+            count++;
+        }
+    }
+    fclose(f);
+    return count;
+}
+
+// function to read data student from txt file
+void load_students(char *filename, Student students[]) {
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) {
+        printf("Can not open file students.txt\n");
+        return;
+    }
+
+    int i = 0;
+    while (!feof(f)) {
+        fscanf(f, "%d %s %f", &students[i].sid, students[i].name,
+               &students[i].gpa);
+        i++;
+    }
+    fclose(f);
+}
+
+// sort students by increasing name (Selection sort)
+void sort_by_name(Student s[], int n) {
+    // YOUR CODE HERE...
+    
+}
+
+int main() {
+    char *filename = "students.txt";
+    int n = count_lines(filename);
+    Student students[n];
+    printf("Input:\n");
+    load_students(filename, students);
+    print_students(students, n);
+    // gọi hàm sắp xếp
+    sort_by_name(students, n);
+    printf("Sorted Output:\n");
+    print_students(students, n);
+}
